3a9b30a3d38cb5ce7776b5998a876af909f086a0
hanguozhi <hanguozhi@bestcode.com>
2019-02-20 16:07:14 +0800

    学习 async/await  / vue-minix

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -27,25 +27,72 @@
 import vHeader from '@/components/header';
 import vscroll from '@/components/scroll';
 import add from '@/utils/add.js';
-const mulity = require('../utils/mulity.js')
+
 export default {
     components:{
         'v-header': vHeader,
         'v-scroll': vscroll
     },
     mounted () {
-
+        this.run(1,3,4,5,6);
     },
     methods: {
-        jump() {
-            this.$router.push({
-                name: 'about',
-                params: {
-                    name: 'abuot'
-                }
+        queryData_1() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve('queryData_1')
+                }, 2000)
             })
+        },
+        queryData_2() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve('queryData_2')
+                }, 2000)
+            })
+        },
+        queryData_3() {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve('queryData_3')
+                }, 2000)
+            })
+        },
+        async run () {
+            console.log(...arguments);
+            // const data_1 = await this.queryData_1();
+            // console.log(data_1) // 2秒后打印 data_1
+            // const data_2 = await this.queryData_2();
+            // console.log(data_2) // 4秒后打印 data_2
+            // const data_3 = await this.queryData_3();
+            // console.log(data_3) // 6秒后打印 data_3
         }
     }
 }
+
+async function getData_1 () {
+    return '100'
+}
+
+// function getData_2 () {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve('200')
+//         }, 2000)
+//     })
+// }
+
+// async  function run () {
+
+//     const data_1 = await getData_1();
+//     console.log(data_1);
+
+//     const data_2 = await getData_2();
+//     console.log(data_2);
+// }
+
+// run ();
+
+
 </script>
 
--- src/pages/me.vue
+++ src/pages/me.vue
@@ -4,4 +4,96 @@
 <script>
 export default {
 }
+
+// // minix.js
+// export default {
+//     data () {
+//         return {
+//             name: 'minix',
+//             minixName: 'minixObj',
+//             flag: false
+//         }
+//     },
+//     mounted() {
+//         console.log('minixMounted');
+//     },
+//     methods: {
+//         speak() {
+//             console.log('this is minix');
+//         },
+//         getData() {
+//             return '100';
+//         }
+//     }
+// }
+
+// // todo.vue
+// import myMinix from './minix';
+
+// export default {
+//     data () {
+//         return {
+//             name: 'todo',
+//             lists: [1, 2, 3, 4]
+//         }
+//     },
+    
+//     mounted() {
+//         console.log('todoMounted');
+//     },
+//     minixs: [myMinix], // todo.vue 中声明minix 进行混合
+//     methods: {
+//         speak () {
+//             console.log('this is todo');
+//         },
+//         submit() {
+//             console.log('submit');
+//         },
+//     }
+// }
+// //==========
+
+// // 最终得到的结果
+
+// //==========
+
+// export default {
+//     data () {
+//         return {
+//             name: 'todo', // 共同有的data， 最后保留自己的data
+//             lists: [1, 2, 3, 4], // 自己独有的，保留
+//             minixName: 'minixObj', // todo没有的，会被添加进来
+//             flag: false // todo没有的，会被添加进来
+//         }
+//     },
+    
+//     mounted() {
+//         // 在钩子函数中的， 会被合并到todo.vue 的钩子函数中， minix中的代码在前，自己的在后
+//         console.log('minixMounted');
+//         console.log('todoMounted');
+//     },
+//     methods: {
+//         // 同时有的方法， 会被封装为一个数组， 先执行minix中的，后执行todo自己的
+//         speak () {
+//             [
+//                 function() {
+//                     console.log('this is minix');
+//                 },
+//                 function() {
+//                     console.log('this is todo');
+//                 }
+//             ].forEach(item => {
+//                 item();
+//             })
+//         },
+//         // 自己独有的，保留
+//         submit() {
+//             console.log('submit');
+//         },
+//         // 自己没有的方法会被添加进来
+//         getData() {
+//             return '100';
+//         }
+//     }
+// }
 </script>
