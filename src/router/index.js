0070ed1bd169238567955dde0fb7277f956cbe45
hanguozhi <hanguozhi@bestcode.com>
2019-02-20 11:59:58 +0800

    路由按需加载分块打包写法

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import home from '@/pages/home'
-import about from '@/pages/about'
-import HelloWorld from '@/components/HelloWorld'
+// import home from '@/pages/home'
+// import about from '@/pages/about'
+// import HelloWorld from '@/components/HelloWorld'
+
+// 按需加载, 分开打包 写法
+// const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+const home = r => require.ensure([], () => r(require('@/pages/home')), 'home')
+const about = r => require.ensure([], () => r(require('@/pages/about')), 'about')
+const HelloWorld = r => require.ensure([], () => r(require('@/components/HelloWorld')), 'HelloWorld')
+
 
 Vue.use(Router)
 /* eslint-disable */
